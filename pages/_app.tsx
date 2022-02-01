12261fe346bce229f875686f518aab03d6707e19
mariandotg <marianguillaume.m@gmail.com>
2022-02-01 11:58:12 -0300

    Created GlobalContext and modified _app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
+import App from "next/app";
 import type { AppProps } from "next/app";
+import { createContext } from "react";
+import { fetchAPI } from "../lib/api";
 import "./_app.css";
+import Head from "next/head";
+
+export const GlobalContext = createContext({});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const { global } = pageProps;
+
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <GlobalContext.Provider value={global}>
+        <Component {...pageProps} />
+      </GlobalContext.Provider>
+    </>
+  );
+}
+
+MyApp.getInitialProps = async (context: any) => {
+  const appProps = await App.getInitialProps(context);
+  const global = await fetchAPI("/global");
+  return { ...appProps, pageProps: { global } };
 }
 
 export default MyApp;
